8b48eda2b0b0f64ae137d8c05fa2d7914df38764
yangyigit <1187161570@qq.com>
2020-10-18 21:45:58 +0800

    Update IndexController.php

--- code/app/Controller/Admin/IndexController.php
+++ code/app/Controller/Admin/IndexController.php
@@ -87,12 +87,18 @@ class IndexController extends BaseController
     }
 
     public function test(){
+        /*        $resUser = [
+          ['status' => 1]  ,
+          ['status' => 0]  ,
+          ['status' => 1]
+        ];*/
         $resUser = Db::table('admin')->get();
-        foreach ($resUser as $k=>$item) {
-            $resUser[$k]['status'] = adminStatusType($item['status']);
-            $resUser[$k]['is_admin'] = '管理员';
+
+        foreach ($resUser as &$item) {
+            $item['status'] = adminStatusType($item['status']);
+            $item['is_admin'] = '管理员';
         }
-       var_dump($resUser);
+       return $resUser;
     }
 
 }
